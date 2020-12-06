645eeaacc9ff2df4192e75eed184606d6327905c
Ali Shah <s.ashah@outlook.com>
2020-12-05 23:55:17 -0500

    [Add] Header component complete

---
+++ src/Title.js
@@ -0,0 +1,12 @@
+// import React from 'react';
+
+function Title() {
+  return (
+    <div className="Title">
+      <h1>SFPOPOS</h1>
+      <div className="Title-Subtitle"> San Francisco's Privately Owned Public Open Spaces</div>
+    </div>
+  );
+}
+
+export default Title;
